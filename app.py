838a8ffc88c319c84b5f72e2e02dd6055727ce1f
Lucas <lucas.o@live.com>
2018-11-22 20:54:26 -0200

    updated

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 from flask import Flask
 app = Flask(__name__)
 
+import random
+
+#Setup database connection
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client['rosa_database']
+complaint = db['complaint']
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
+def get_random_id():
+    return random.randint(10000, 99999)
 
-def nova_queixa():
-    pass
+def get_new_id():
+    random_id = get_random_id()
+    while complaint.find_one({"_id": random_id}):
+        random_id = get_random_id()
+        
+    new_id = complaint.insert_one({"_id": random_id}).inserted_id
+    
+    return new_id
 
-def status_queixa():
-    pass
+@app.route("/complaint")
+def new_complaint():
+    new_id = get_new_id()
+    return str(new_id)
 
-def atualizar_queixa():
-    pass
+
+# def status_queixa():
+#     pass
+
+# def atualizar_queixa():
+#     pass
 
 
 if __name__ == '__main__':
