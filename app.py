fe1bbfe09f77c79ec9eac272a901fa39fee3054e
Lucas <lucas.o@live.com>
2018-11-24 13:33:18 -0200

    updated

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
 app = Flask(__name__)
 
 import random
+
+from functools import wraps
+
+import os
 
 #Setup database connection
 from pymongo import MongoClient
@@ -10,9 +14,24 @@ db = client['rosa_database']
 complaint = db['complaint']
 
 
+#Setup api key and decorator
+APPKEY_HERE = os.getenv("ROSA_CRUD_KEY", 'ROSABOT')
+
+# The actual decorator function
+def require_appkey(view_function):
+    @wraps(view_function)
+    # the new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        if request.args.get('key') and request.args.get('key') == APPKEY_HERE:
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def root_page():
+    return "ROSABOT"
 
 def get_random_id():
     return random.randint(10000, 99999)
@@ -26,17 +45,53 @@ def get_new_id():
     
     return new_id
 
-@app.route("/complaint")
-def new_complaint():
+
+@app.route("/complaint/create", methods=['GET'])
+@require_appkey
+def complaint_new():
     new_id = get_new_id()
     return str(new_id)
 
 
-# def status_queixa():
-#     pass
+@app.route("/complaint/list")
+@require_appkey
+def complaint_list():
+    id_list = list()
 
-# def atualizar_queixa():
-#     pass
+    for c_obj in complaint.find():
+         id_list.append(c_obj['_id'])
+
+    return jsonify(id_list)
+
+
+@app.route("/complaint/update/<int:complaint_id>", methods=['POST'])
+@require_appkey
+def complaint_update(complaint_id):
+    request_json = request.get_json()
+    
+    if request_json == None:
+        return jsonify({'error':"No valid JSON body sent."})
+
+    complaint_obj = complaint.find_one({"_id": complaint_id})
+    if complaint_obj == None:
+        return jsonify({'error':"Complaint id not found."})
+        
+    complaint.update_one({'_id': complaint_id}, {'$set': request_json})
+
+    return jsonify({'status':"updated"})
+
+
+@app.route("/complaint/status/<int:complaint_id>", methods=['GET'])
+@require_appkey
+def complaint_status(complaint_id):
+
+    complaint_obj = complaint.find_one({"_id": complaint_id})
+
+    if complaint_obj == None:
+        return jsonify({'error':"Complaint id not found."})
+
+    return jsonify(complaint_obj)
+
 
 
 if __name__ == '__main__':
